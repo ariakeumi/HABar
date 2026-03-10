e4a2d7a22ee983a7f1e9bf7199032132b910655f
ariakeumi <Ariakeumi@Gmail.com>
2026-03-10 13:08:43 +0800

    Initial Commit

---
+++ HABar/HABarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HABarApp.swift
+//  HABar
+//
+//  Created by 陈土豆 on 2026/3/10.
+//
+
+import SwiftUI
+
+@main
+struct HABarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
